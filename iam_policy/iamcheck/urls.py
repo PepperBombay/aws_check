53342b82bd150159ca28559592c6e3a7decc234b
mingyu <sjfla3197@gmail.com>
2023-09-15 17:30:20 +0900

    테스트 파일 삭제 -신민규

--- iam_policy/iamcheck/urls.py
+++
@@ -1,7 +0,0 @@
-# iamcheck/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('RootAccount_MFA_Check/', views.RootAccount_MFA_Check, name='RootAccount_MFA_Check'),
-]
