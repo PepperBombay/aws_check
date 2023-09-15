713c992566dd7ae40134612428df1eae0df93083
mingyu <sjfla3197@gmail.com>
2023-09-15 22:50:55 +0900

    iam 정책 추가

--- CloudMain/urls.py
+++ CloudMain/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from aws_subnet_check import views, include
+from django.urls import path, include
+from aws_subnet_check import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- aws_subnet_check_project/aws_subnet_check/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AwsSubnetCheckConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'aws_subnet_check'
