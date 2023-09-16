78d8716bbbf5a1ba597b24ecdceb6dac0a0e08a3
김지은 <kimilyn4@naver.com>
2023-09-16 21:05:18 +0900

    Update urls.py

--- aws_ec2_SecurityGroup/urls.py
+++ aws_ec2_SecurityGroup/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('security/<str:security_group_id>/', views.get_security_group_info, name='get_security_group_info'),
+    path('<str:security_group_id>/', views.get_security_group_info, name='get_security_group_info'),
 ]
