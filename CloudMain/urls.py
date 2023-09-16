0328cb8f49f058ad5b86f6d1f782fa7099f2fe12
김지은 <kimilyn4@naver.com>
2023-09-16 21:23:26 +0900

    Update ec2 security group urls.py

--- CloudMain/urls.py
+++ CloudMain/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('aws-subnet-check/<str:vpc_id>/<str:aws_region>/', views.check_vpc_subnets, name='check_vpc_subnets'),
     path('iam_policy/', include('iam_policy.urls')),
     path('aws_s3_PublicAccess/', include('aws_s3_PublicAccess.urls')),
+    path('aws_ec2_SecurityGroup/', include(aws_ec2_SecurityGroup.urls))
 ]
