4160be4f9b53b03b9750490af9579ebfb0c2d639
Kim jieun <kimilyn4@naver.com>
2023-09-20 06:21:00 +0900

    EC2_SG 보안그룹 경우의 수 추가, 경고 메시지 추가

--- aws_ec2_SecurityGroup/views.py
+++ aws_ec2_SecurityGroup/views.py
@@ -15,23 +15,34 @@ def get_security_group_info(request, instance_id):
                     for sg in instance['SecurityGroups']:
                         security_group_ids.append(sg['GroupId'])
 
-        # 보안 그룹 정보 가져오기
-        security_group = ec2_client.describe_security_groups(GroupIds=security_group_ids)['SecurityGroups'][0]
+        if not security_group_ids:
+            return JsonResponse({'warning': '보안 그룹이 존재하지 않습니다. 보안 그룹을 생성하세요.'})
 
-        # 정보 추출
-        group_id = security_group['GroupId']
-        group_name = security_group['GroupName']
-        inbound_rules = security_group.get('IpPermissions', [])
-        outbound_rules = security_group.get('IpPermissionsEgress', [])
+        # 모든 보안 그룹 정보 가져오기
+        security_groups = ec2_client.describe_security_groups(GroupIds=security_group_ids)['SecurityGroups']
 
-        # 추출한 정보를 JSON으로 반환
-        data = {
-            'group_id': group_id,
-            'group_name': group_name,
-            'inbound_rules': inbound_rules,
-            'outbound_rules': outbound_rules,
-        }
+        security_group_data = []
+        for security_group in security_groups:
+            group_id = security_group['GroupId']
+            group_name = security_group['GroupName']
+            inbound_rules = security_group.get('IpPermissions', [])
+            outbound_rules = security_group.get('IpPermissionsEgress', [])
 
-        return JsonResponse(data)
+            group_data = {
+                'group_id': group_id,
+                'group_name': group_name,
+                'inbound_rules': inbound_rules,
+                'outbound_rules': outbound_rules,
+            }
+            security_group_data.append(group_data)
+            # 경고 메시지 추가
+            if any(rule.get('IpRanges', [{'CidrIp': '0.0.0.0/0'}])[0]['CidrIp'] == '0.0.0.0/0' for rule in
+                   inbound_rules):
+                warning_message = '경고: 인바운드 규칙이 모든 IP 허용으로 설정되어 있습니다. 최소한의 IP만 허용하십시오.'
+                security_group_data.append({'warning': warning_message})
+
+
+
+        return JsonResponse({'security_groups': security_group_data})
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=500)
